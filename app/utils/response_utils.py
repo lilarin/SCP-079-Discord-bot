2a57cbc077b1cc1fde1d21648ff80684a6c59adb
lilarin <sakuraimain1192@gmail.com>
2025-06-15 02:13:46 +0300

    feat: implement response utils to manage responses

---
+++ app/utils/response_utils.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from disnake import MessageFlags, File, Embed
+
+
+class ResponseUtils:
+    @staticmethod
+    async def wait_for_response(interaction) -> None:
+        await interaction.send(f"Очікуйте...", flags=MessageFlags(suppress_notifications=True))
+
+    @staticmethod
+    async def wait_for_ephemeral_response(interaction) -> None:
+        await interaction.send(f"Очікуйте...", flags=MessageFlags(ephemeral=True))
+
+    @staticmethod
+    async def send_response(
+            interaction,
+            message: Optional[str] = None,
+            embed: Optional[Embed] = None
+    ) -> None:
+        if embed:
+            await interaction.edit_original_response(content=message, embed=embed)
+        else:
+            await interaction.edit_original_response(content=message)
+
+    @staticmethod
+    async def send_ephemeral_response(interaction, message: Optional[str] = None) -> None:
+        await interaction.send(message, flags=MessageFlags(ephemeral=True))
+
+
+response_utils = ResponseUtils()
