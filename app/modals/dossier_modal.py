9372a75642246f4d3d8d0e53301855b9e638796c
lilarin <sakuraimain1192@gmail.com>
2025-06-18 01:55:26 +0300

    feat: implement dossier modal

---
+++ app/modals/dossier_modal.py
@@ -0,0 +1,40 @@
+import disnake
+
+from app.models import User
+from app.utils.response_utils import response_utils
+
+
+class DossierModal(disnake.ui.Modal):
+    def __init__(self, user: disnake.User, db_user: User) -> None:
+        self.dossier = self._format_dossier(db_user)
+
+        components = [
+            disnake.ui.TextInput(
+                label="Редагування досьє",
+                placeholder=self.dossier,
+                custom_id="dossier",
+                style=disnake.TextInputStyle.paragraph,
+                min_length=20,
+                max_length=1024,
+            ),
+        ]
+
+        super().__init__(
+            title="",
+            custom_id="dossierModal",
+            components=components,
+        )
+        self.user = user
+        self.db_user = db_user
+
+    @staticmethod
+    def _format_dossier(db_user: User) -> str:
+        return db_user.dossier[:97] + "..." if db_user else "..."
+
+    async def callback(self, interaction: disnake.ModalInteraction) -> None:
+        new_dossier = interaction.text_values.get("dossier")
+
+        self.db_user.dossier = new_dossier
+        await self.db_user.save()
+
+        await response_utils.send_ephemeral_response(interaction, "Досьє оновлено!")
