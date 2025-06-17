df7b6a2d3b437d837947dce6fceef60c88ba73dc
lilarin <sakuraimain1192@gmail.com>
2025-06-18 01:56:11 +0300

    refactor(style): add type annotations

--- app/utils/keycard_utils.py
+++ app/utils/keycard_utils.py
@@ -1,38 +1,45 @@
 from typing import Optional
 
-from disnake import Embed, File
+from disnake import Embed, File, Role
 
 
 class KeyCardUtils:
     @staticmethod
-    async def process_username(user_name):
+    async def process_username(user_name: str) -> str:
         user_name = user_name.upper()
         if len(user_name) > 14:
             user_name = (user_name[:12].strip() + "..")
         return user_name
 
     @staticmethod
-    async def get_user_code(timestamp: float):
+    async def get_user_code(timestamp: float) -> str:
         return "-".join(str(round(timestamp, 1)).split("."))
 
     @staticmethod
-    async def format_new_user_embed(user_mention, card, color):
+    async def format_new_user_embed(user_mention: str, card: File, color: int) -> Embed:
         embed = Embed(
             description=f"Вітаємо {user_mention} у складі співробітників фонду!",
             color=color
         )
         embed.set_image(file=card)
+
         return embed
 
     @staticmethod
-    async def format_user_embed(card: File, color: int, dossier: Optional[str] = None):
+    async def format_user_embed(
+            card: File, color: int, dossier: Optional[str] = None, role: Optional[Role] = None
+    ) -> Embed:
         embed = Embed(
             title="Інформація про співробітника фонду",
             color=color
         )
         embed.set_image(file=card)
+
+        if role:
+            embed.add_field(name="Посада:", value=role.mention, inline=False)
         if dossier:
             embed.add_field(name="Досьє:", value=dossier, inline=False)
+
         return embed
 
 
