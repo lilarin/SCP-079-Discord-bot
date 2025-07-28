3e1483d66b0fc10877bbdcbda985ad51076adf69
lilarin <sakuraimain1192@gmail.com>
2025-07-28 03:14:18 +0300

    feat: improve error handling and logging

--- app/bot.py
+++ app/bot.py
@@ -2,6 +2,7 @@ import asyncio
 import io
 import random
 
+import asyncpg
 import disnake
 from disnake.ext import commands
 
@@ -9,6 +10,7 @@ from app.config import config, logger
 from app.modals.dossier_modal import DossierModal
 from app.models import User
 from app.services.keycard_service import keycard_service
+from app.services.scp_objects_service import scp_objects_service
 from app.utils.keycard_utils import keycard_utils
 from app.utils.response_utils import response_utils
 from app.utils.time_utils import time_utils
@@ -18,6 +20,7 @@ bot = commands.InteractionBot(intents=disnake.Intents.all())
 
 @bot.event
 async def on_ready():
+    await scp_objects_service.update_scp_objects()
     timestamp = await time_utils.get_normalised()
     logger.info(f"[{timestamp}] Виконано вхід як {bot.user}")
     await asyncio.sleep(1)
@@ -73,7 +76,11 @@ async def on_member_join(member):
 
     await member.guild.system_channel.send(embed=embed)
 
-    await User.get_or_create(user_id=member.id)
+    try:
+        await User.get_or_create(user_id=member.id)
+    except asyncpg.exceptions.InternalServerError as exception:
+        timestamp = await time_utils.get_normalised()
+        logger.error(f"[{timestamp}] {exception}")
 
 
 @bot.slash_command(name="картка", description="Переглянути картку співробітника фонду")
@@ -105,17 +112,23 @@ async def view_card(
         avatar_decoration,
     )
 
-    db_user, created = await User.get_or_create(user_id=member.id)
-    top_role = member.top_role
+    try:
+        db_user, created = await User.get_or_create(user_id=member.id)
+        top_role = member.top_role
 
-    embed = await keycard_utils.format_user_embed(
-        card=card,
-        color=template.embed_color,
-        dossier=db_user.dossier if not created else None,
-        role=top_role if top_role != member.guild.default_role else None,
-    )
+        embed = await keycard_utils.format_user_embed(
+            card=card,
+            color=template.embed_color,
+            dossier=db_user.dossier if not created else None,
+            role=top_role if top_role != member.guild.default_role else None,
+        )
 
-    await response_utils.send_response(interaction, embed=embed)
+        await response_utils.send_response(interaction, embed=embed)
+
+    except asyncpg.exceptions.InternalServerError as exception:
+        await response_utils.send_response(interaction, "Виникла помилка під час отримання користувача")
+        timestamp = await time_utils.get_normalised()
+        logger.error(f"[{timestamp}] {exception}")
 
 
 @bot.slash_command(name="досьє", description="Заповнити своє досьє")
@@ -126,71 +139,70 @@ async def view_card(interaction: disnake.ApplicationCommandInteraction):
         modal=DossierModal(user=interaction.user, db_user=db_user)
     )
 
-
-@bot.slash_command(name="пнути", description="???")
-@commands.has_permissions(administrator=True)
-async def spam(
-        interaction: disnake.ApplicationCommandInteraction,
-        user: disnake.User = commands.Param(description="Оберіть користувача"),
-        threads: int = commands.Param(
-            description="Кількість гілок для надсилання повідомлень",
-            default=25, max_value=25
-        ),
-        messages: int = commands.Param(
-            description="Кількість надісланих повідомлень у кожній гілці",
-            default=400, max_value=4000
-        ),
-        delete_existing_threads: bool = commands.Param(
-            description="Видалити існуючі гілки в каналі?",
-            default=False
-        ),
-):
-    await response_utils.wait_for_ephemeral_response(interaction)
-
-    if interaction.channel.type != disnake.ChannelType.text:
-        await response_utils.send_response(
-            interaction,
-            message=f"Команду можна використовувати лише в **текстових** каналах"
-        )
-        return
-
-    if user.bot:
-        await response_utils.send_response(
-            interaction,
-            message=f"Команду не можна використовувати на ботах"
-        )
-        return
-
-    await response_utils.send_response(
-        interaction,
-        message=f"{user.mention} поплачет"
-    )
-
-    channel = interaction.channel
-    if delete_existing_threads:
-        for channel_thread in channel.threads:
-            await channel_thread.delete()
-
-    message_content = f"{user.mention}[.](https://imgur.com/1bS0ahP.png)"
-    thread_name = "⠀̛"
-
-    threads_to_create = [
-        channel.create_thread(
-            name=thread_name,
-            type=disnake.ChannelType.private_thread,
-            invitable=False
-        )
-        for _ in range(threads)
-    ]
-
-    created_threads = await asyncio.gather(*threads_to_create)
-
-    async def work(thread):
-        for _ in range(messages):
-            await thread.send(content=message_content)
-
-    tasks = [asyncio.create_task(work(thread)) for thread in created_threads]
-    await asyncio.gather(*tasks)
-
-    for thread in created_threads:
-        await thread.delete()
+# @bot.slash_command(name="пнути", description="???")
+# @commands.has_permissions(administrator=True)
+# async def spam(
+#         interaction: disnake.ApplicationCommandInteraction,
+#         user: disnake.User = commands.Param(description="Оберіть користувача"),
+#         threads: int = commands.Param(
+#             description="Кількість гілок для надсилання повідомлень",
+#             default=25, max_value=25
+#         ),
+#         messages: int = commands.Param(
+#             description="Кількість надісланих повідомлень у кожній гілці",
+#             default=400, max_value=4000
+#         ),
+#         delete_existing_threads: bool = commands.Param(
+#             description="Видалити існуючі гілки в каналі?",
+#             default=False
+#         ),
+# ):
+#     await response_utils.wait_for_ephemeral_response(interaction)
+#
+#     if interaction.channel.type != disnake.ChannelType.text:
+#         await response_utils.send_response(
+#             interaction,
+#             message=f"Команду можна використовувати лише в **текстових** каналах"
+#         )
+#         return
+#
+#     if user.bot:
+#         await response_utils.send_response(
+#             interaction,
+#             message=f"Команду не можна використовувати на ботах"
+#         )
+#         return
+#
+#     await response_utils.send_response(
+#         interaction,
+#         message=f"{user.mention} поплачет"
+#     )
+#
+#     channel = interaction.channel
+#     if delete_existing_threads:
+#         for channel_thread in channel.threads:
+#             await channel_thread.delete()
+#
+#     message_content = f"{user.mention}[.](https://imgur.com/1bS0ahP.png)"
+#     thread_name = "⠀̛"
+#
+#     threads_to_create = [
+#         channel.create_thread(
+#             name=thread_name,
+#             type=disnake.ChannelType.private_thread,
+#             invitable=False
+#         )
+#         for _ in range(threads)
+#     ]
+#
+#     created_threads = await asyncio.gather(*threads_to_create)
+#
+#     async def work(thread):
+#         for _ in range(messages):
+#             await thread.send(content=message_content)
+#
+#     tasks = [asyncio.create_task(work(thread)) for thread in created_threads]
+#     await asyncio.gather(*tasks)
+#
+#     for thread in created_threads:
+#         await thread.delete()
