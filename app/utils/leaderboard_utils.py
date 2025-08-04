750dd50d18e2062240ad020b5c0c11c3bd1ab132
lilarin <sakuraimain1192@gmail.com>
2025-08-04 21:02:36 +0300

    feat: implement method to add buttons

--- app/utils/leaderboard_utils.py
+++ app/utils/leaderboard_utils.py
@@ -30,5 +30,47 @@ class LeaderboardUtils:
         embed.description += f"\n-# {hint}"
         return embed
 
+    @staticmethod
+    async def init_leaderboard_buttons(
+            current_page_text: str = "1",
+            disable_first_page_button: bool = False,
+            disable_previous_page_button: bool = False,
+            disable_next_page_button: bool = False,
+            disable_last_page_button: bool = False,
+    ) -> disnake.ui.ActionRow:
+        first_page_button = disnake.ui.Button(
+            style=disnake.ButtonStyle.grey,
+            label="🡸",
+            custom_id="first_page_button",
+            disabled=disable_first_page_button,
+        )
+        previous_page_button = disnake.ui.Button(
+            style=disnake.ButtonStyle.grey,
+            label="❮",
+            custom_id="previous_page_button",
+            disabled=disable_previous_page_button,
+        )
+        current_page_button = disnake.ui.Button(
+            style=disnake.ButtonStyle.grey,
+            label=current_page_text,
+            custom_id="current_page_button",
+            disabled=True,
+        )
+        next_page_button = disnake.ui.Button(
+            style=disnake.ButtonStyle.grey,
+            label="❯",
+            custom_id="next_page_button",
+            disabled=disable_next_page_button,
+        )
+        last_page_button = disnake.ui.Button(
+            style=disnake.ButtonStyle.grey,
+            label="🡺",
+            custom_id="last_page_button",
+            disabled=disable_last_page_button,
+        )
+        return disnake.ui.ActionRow(
+            first_page_button, previous_page_button, current_page_button, next_page_button, last_page_button,
+        )
+
 
 leaderboard_utils = LeaderboardUtils()
