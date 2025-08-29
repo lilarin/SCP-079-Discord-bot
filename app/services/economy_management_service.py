802efa610fe0a0c3dc2092fa5a8ca5fe6e214f1c
lilarin <sakuraimain1192@gmail.com>
2025-08-29 03:10:46 +0300

    feat(style): update service to create balance message with user avatar. Minor style improvements

--- app/services/economy_management_service.py
+++ app/services/economy_management_service.py
@@ -1,49 +1,52 @@
 from typing import Tuple
 
+from disnake import User
 from tortoise.exceptions import DoesNotExist
 from tortoise.expressions import Q
 from tortoise.transactions import in_transaction
 
-from app.core.models import User
+from app.core.models import User as UserModel
 from app.utils.ui_utils import ui_utils
 
 
 class EconomyManagementService:
     @staticmethod
     async def set_user_balance(user_id: int, amount: int) -> None:
-        user, is_created = await User.get_or_create(user_id=user_id)
+        user, is_created = await UserModel.get_or_create(user_id=user_id)
         if not user:
             raise DoesNotExist(f"Користувача з ID {user_id} не знайдено")
         await user.set_balance(amount)
 
     @staticmethod
     async def set_user_reputation(user_id: int, amount: int) -> None:
-        user, is_created = await User.get_or_create(user_id=user_id)
+        user, is_created = await UserModel.get_or_create(user_id=user_id)
         if not user:
             raise DoesNotExist(f"Користувача з ID {user_id} не знайдено")
         await user.set_reputation(amount)
 
     @staticmethod
     async def update_user_balance(user_id: int, amount: int) -> None:
-        user, is_created = await User.get_or_create(user_id=user_id)
+        user, is_created = await UserModel.get_or_create(user_id=user_id)
         if not user:
             raise DoesNotExist(f"Користувача з ID {user_id} не знайдено")
         await user.update_balance(amount)
 
     @staticmethod
-    async def create_user_balance_message(user_id: int) -> Tuple[int, int]:
-        user, is_created = await User.get_or_create(user_id=user_id)
-        higher_ranking_users_count = await User.filter(
-            Q(reputation__gt=user.reputation) |
-            Q(reputation=user.reputation, user_id__lt=user.user_id)
+    async def create_user_balance_message(user: User) -> Tuple[int, int]:
+        db_user, is_created = await UserModel.get_or_create(user_id=user.id)
+        higher_ranking_users_count = await UserModel.filter(
+            Q(reputation__gt=db_user.reputation) |
+            Q(reputation=db_user.reputation, user_id__lt=db_user.user_id)
         ).count()
         position = higher_ranking_users_count + 1
 
-        return await ui_utils.format_balance_embed(user.balance, user.reputation, position)
+        user_avatar_url = user.display_avatar.url if hasattr(user, "display_avatar") else user.avatar.url
+
+        return await ui_utils.format_balance_embed(user_avatar_url, db_user.balance, db_user.reputation, position)
 
     @staticmethod
     async def reset_users_reputation() -> None:
-        await User.all().update(reputation=0)
+        await UserModel.all().update(reputation=0)
 
     @staticmethod
     async def transfer_balance(sender_id: int, receiver_id: int, amount: int) -> Tuple[bool, str]:
@@ -54,14 +57,14 @@ class EconomyManagementService:
             return False, "Сума переводу повинна бути більше нуля"
 
         async with in_transaction():
-            sender, _ = await User.get_or_create(user_id=sender_id)
+            sender, _ = await UserModel.get_or_create(user_id=sender_id)
             if sender.balance < amount:
                 return False, (
                     "У вас недостатньо коштів для переказу\n"
                     f"-# Поточний баланс – {sender.balance} 💠"
                 )
 
-            receiver, _ = await User.get_or_create(user_id=receiver_id)
+            receiver, _ = await UserModel.get_or_create(user_id=receiver_id)
 
             sender.balance -= amount
             await sender.save(update_fields=["balance"])
